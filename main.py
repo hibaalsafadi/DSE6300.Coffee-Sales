ba36bb688b0ccb6695b7416f909c4914d822a8a3
Allison Gohl <allison@Allisons-MacBook-Pro-2.local>
2022-04-18 16:17:48 -0400

    added a plot

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 import pandas as pd
 from pyspark.sql import functions as f
 
-from transactionAnalysis import run_retail_analysis
+from transactionAnalysis import *
 
 
 def read_in_data(user):
@@ -27,22 +27,29 @@ def read_in_data(user):
         .option("driver", "org.postgresql.Driver") \
         .load()
 
-    df.dropna
-    df.printSchema()
     df = df.where(df.transaction_id != 'transaction_id')
     df.describe().toPandas().transpose()
+    df = df.withColumn("transaction_id", df.quantity.cast("Integer"))
+    df = df.withColumn("sales_outlet_id", df.quantity.cast("Integer"))
+    df = df.withColumn("staff_id", df.quantity.cast("Integer"))
+    df = df.withColumn("order", df.quantity.cast("Integer"))
+    df = df.withColumn("product_id", df.quantity.cast("Integer"))
+    df = df.withColumn("line_item_id", df.quantity.cast("Integer"))
     df = df.withColumn("quantity", df.quantity.cast("Float"))
     df = df.withColumn("unit_price", df.unit_price.cast("Float"))
     df = df.withColumn("line_item_amount", df.line_item_amount.cast("Float"))
     df = df.withColumn("transaction_date_time",
                        f.concat(df.transaction_date, f.lit(" "), df.transaction_time))
-    df = df.withColumn("transaction_date_time", f.from_unixtime(f.unix_timestamp('transaction_date_time',
-                                                                                 "yyyy-MM-dd HH:mm:ss"))
-                       .alias("transaction_date_time"))
+    df = df.withColumn('transaction_date_time', f.from_unixtime(f.unix_timestamp('transaction_date_time',
+                                                                                 'yyyy-MM-dd HH:mm:ss')))
+    df = df.dropna(how='any')
+    df = df.dropDuplicates()
+    df.printSchema()
     return df
 
 
 if __name__ == '__main__':
     data = read_in_data(False)
     # Uncomment/ comment out algorithm to run
-    run_retail_analysis(data)
+    # run_fp_growth(data)
+    plotLineItemAmount(data)
