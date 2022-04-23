52045f06aa2016e1be8df6ba3063c244512986db
hibaalsafadi <hi8740>
2022-04-22 21:37:33 -0400

    starting code for AA

---
+++ ApiriorAlgorithim.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+from mlxtend.frequent_patterns import apriori, association_rules
+
+# Changing the working location to the location of the file
+#cd /Users/hibaalsafadi/Desktop/ApiriorAlgorithim
+
+
+
+
+#Loading product data
+product = pd.read_csv('product.csv')
+print(product.head(6))
+
+#Exploring the column of the product
+print(product.columns)
+
+#Exploring the different regions of transactions
+
+
+
+
+# Loading the Data
+#data = pd.read_csv('customer.csv')
+#print(data.head(7))
+
+
+# Exploring the columns of the data
+#print(data.columns)
+
+# Exploring the different regions of transactions
+# data.Country.unique()
+
+
+
+#df = pd.read_csv('data.csv', index_col=0)
+
+
+
+
+
+
+#########COMPARING YES/NO TRUE FALSE ###########
+#import pandas as pd
+#cereal_df = pd.read_csv("/tmp/tmp07wuam09/data/cereal.csv")
+#cereal_df2 = pd.read_csv("data/cereal.csv")
+
+# Are they the same?
+#print(pd.DataFrame.equals(cereal_df, cereal_df2))
