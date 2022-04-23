9c9ce6c463ad6954ac6206db0c853557f6e30adb
hibaalsafadi <hi8740>
2022-04-22 22:09:26 -0400

    PyFolders

--- ApiriorAlgorithim.py
+++ ApiriorAlgorithim.py
@@ -7,43 +7,47 @@ from mlxtend.frequent_patterns import apriori, association_rules
 
 
 
+def SR(df):
+    #Loading product data
+    # df = pd.read_csv('.csv')
+    # print(df.head(6))
+    #
+    # #Exploring the column of the product
+    # print(product.columns)
 
-#Loading product data
-product = pd.read_csv('product.csv')
-print(product.head(6))
+    #Exploring the different regions of transactions
 
-#Exploring the column of the product
-print(product.columns)
-
-#Exploring the different regions of transactions
-
-
-
-
-# Loading the Data
-#data = pd.read_csv('customer.csv')
-#print(data.head(7))
-
-
-# Exploring the columns of the data
-#print(data.columns)
-
-# Exploring the different regions of transactions
-# data.Country.unique()
-
-
-
-#df = pd.read_csv('data.csv', index_col=0)
+    df.show()
 
 
 
 
 
 
-#########COMPARING YES/NO TRUE FALSE ###########
-#import pandas as pd
-#cereal_df = pd.read_csv("/tmp/tmp07wuam09/data/cereal.csv")
-#cereal_df2 = pd.read_csv("data/cereal.csv")
+    # Loading the Data
+    #data = pd.read_csv('customer.csv')
+    #print(data.head(7))
 
-# Are they the same?
-#print(pd.DataFrame.equals(cereal_df, cereal_df2))
+
+    # Exploring the columns of the data
+    #print(data.columns)
+
+    # Exploring the different regions of transactions
+    # data.Country.unique()
+
+
+
+    #df = pd.read_csv('data.csv', index_col=0)
+
+
+
+
+
+
+    #########COMPARING YES/NO TRUE FALSE ###########
+    #import pandas as pd
+    #cereal_df = pd.read_csv("/tmp/tmp07wuam09/data/cereal.csv")
+    #cereal_df2 = pd.read_csv("data/cereal.csv")
+
+    # Are they the same?
+    #print(pd.DataFrame.equals(cereal_df, cereal_df2))
