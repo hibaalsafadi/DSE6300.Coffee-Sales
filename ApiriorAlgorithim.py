bba36189cf661865f7795f4fbf1fce25cbe69fdd
hibaalsafadi <hi8740>
2022-04-23 17:58:32 -0400

    Algorithm

--- ApiriorAlgorithim.py
+++ ApiriorAlgorithim.py
@@ -5,7 +5,11 @@ from mlxtend.frequent_patterns import apriori, association_rules
 # Changing the working location to the location of the file
 #cd /Users/hibaalsafadi/Desktop/ApiriorAlgorithim
 
-
+def hot_encode(x):
+    if (x <= 0):
+        return 0
+    if (x >= 1):
+        return 1
 
 def SR(df):
     #Loading product data
@@ -20,9 +24,54 @@ def SR(df):
     df.show()
 
 
+    df = df.toPandas()
+    corM = df.corr()
+    print(corM)
+    # test_df= df.groupby(['transaction_id', 'product_id'])['quantity'].sum()
+    # print(test_df.head(20))
+    # # Transactions done in staff ID
 
+    basket_3 = (df[df['sales_outlet_id'] == 3]
+                .groupby(['transaction_id', 'product_id'])['quantity']
+                .sum().unstack().reset_index().fillna(0)
+                .set_index('transaction_id'))
+    print(type(basket_3))
+    print(basket_3.head(20))
+    basket_encoded = basket_3.applymap(hot_encode)
+    basket_3 = basket_encoded
+    frq_items = apriori(basket_3, min_support=0.01, use_colnames=True)
+    rules = association_rules(frq_items, metric="lift", min_threshold=1)
+    rules = rules.sort_values(['confidence', 'lift'], ascending=[False, False])
+    print(rules.head())
+    print(frq_items.head())
 
+    basket_5 = (df[df['sales_outlet_id'] == 5]
+                .groupby(['transaction_id', 'product_id'])['quantity']
+                .sum().unstack().reset_index().fillna(0)
+                .set_index('transaction_id'))
+    print(type(basket_5))
+    print(basket_5.head(20))
+    basket_encoded = basket_5.applymap(hot_encode)
+    basket_5 = basket_encoded
+    frq_items = apriori(basket_5, min_support=0.01, use_colnames=True)
+    rules = association_rules(frq_items, metric="lift", min_threshold=1)
+    rules = rules.sort_values(['confidence', 'lift'], ascending=[False, False])
+    print(rules.head())
+    print(frq_items.head())
 
+    basket_8 = (df[df['sales_outlet_id'] == 8]
+                .groupby(['transaction_id', 'product_id'])['quantity']
+                .sum().unstack().reset_index().fillna(0)
+                .set_index('transaction_id'))
+    print(type(basket_8))
+    print(basket_8.head(20))
+    basket_encoded = basket_8.applymap(hot_encode)
+    basket_8 = basket_encoded
+    frq_items = apriori(basket_8, min_support=0.01, use_colnames=True)
+    rules = association_rules(frq_items, metric="lift", min_threshold=1)
+    rules = rules.sort_values(['confidence', 'lift'], ascending=[False, False])
+    print(rules.head())
+    print(frq_items.head())
 
     # Loading the Data
     #data = pd.read_csv('customer.csv')
