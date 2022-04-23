62c02cb6b757a1f77483bd52c5f8aca344e72759
gohlam <gohla@umich.edu>
2022-04-23 15:03:05 -0400

    Kafka env (#1)
    * reading data in from postgres and doing some / cleaning updates
    
    * added a plot
    
    * temp workaround for hiba
    
    * update gitignore
    
    * trying to get application to run in docker
    
    * finally got code to run in a docker container
    
    * added kafka and got containers/images to build and run
    
    Co-authored-by: Allison Gohl <allison@Allisons-MacBook-Pro-2.local>
    Co-authored-by: gohlam <allisongohl@gmail.com>

--- kafkaConsumer.py
+++ kafkaConsumer.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 from json import loads
 consumer = KafkaConsumer(
     'results',
-    bootstrap_servers='kafka:9092',
+    bootstrap_servers=['kafka:9092'],
     auto_offset_reset='earliest',
     enable_auto_commit=True,
     group_id='my-group',
