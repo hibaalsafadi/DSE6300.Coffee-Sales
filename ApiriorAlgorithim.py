143290898ceca5a8e2fdc1e1a2d035a79ea9a346
hibaalsafadi <hi8740>
2022-04-24 20:12:22 -0400

    cleaned up code and added remaining buckets

--- ApiriorAlgorithim.py
+++ ApiriorAlgorithim.py
@@ -1,9 +1,5 @@
-import numpy as np
-import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
 
-# Changing the working location to the location of the file
-#cd /Users/hibaalsafadi/Desktop/ApiriorAlgorithim
 
 def hot_encode(x):
     if (x <= 0):
@@ -11,26 +7,10 @@ def hot_encode(x):
     if (x >= 1):
         return 1
 
+
 def SR(df):
-    #Loading product data
-    # df = pd.read_csv('.csv')
-    # print(df.head(6))
-    #
-    # #Exploring the column of the product
-    # print(product.columns)
-
-    #Exploring the different regions of transactions
-
     df.show()
-
-
     df = df.toPandas()
-    corM = df.corr()
-    print(corM)
-    # test_df= df.groupby(['transaction_id', 'product_id'])['quantity'].sum()
-    # print(test_df.head(20))
-    # # Transactions done in staff ID
-
     basket_3 = (df[df['sales_outlet_id'] == 3]
                 .groupby(['transaction_id', 'product_id'])['quantity']
                 .sum().unstack().reset_index().fillna(0)
@@ -73,30 +53,3 @@ def SR(df):
     print(rules.head())
     print(frq_items.head())
 
-    # Loading the Data
-    #data = pd.read_csv('customer.csv')
-    #print(data.head(7))
-
-
-    # Exploring the columns of the data
-    #print(data.columns)
-
-    # Exploring the different regions of transactions
-    # data.Country.unique()
-
-
-
-    #df = pd.read_csv('data.csv', index_col=0)
-
-
-
-
-
-
-    #########COMPARING YES/NO TRUE FALSE ###########
-    #import pandas as pd
-    #cereal_df = pd.read_csv("/tmp/tmp07wuam09/data/cereal.csv")
-    #cereal_df2 = pd.read_csv("data/cereal.csv")
-
-    # Are they the same?
-    #print(pd.DataFrame.equals(cereal_df, cereal_df2))
